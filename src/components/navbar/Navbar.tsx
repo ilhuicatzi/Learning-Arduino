dc38a4503075820a534ad4bca0370b340ac58b4b
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-05-12 23:46:27 -0600

    refactor Navbar and update CSS variables for improved styling consistency

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,27 +10,27 @@ function Navbar() {
       <div className="flex justify-center items-center">
         <ul className="flex gap-4">
           <li>
-            <Link to="/" className="text-blue-500 hover:text-blue-700">
+            <Link to="/" className="text-primary font-semibold">
               Inicio
             </Link>
           </li>
           <li>
-            <Link to="/arduino-basics" className="text-blue-500 hover:text-blue-700">
+            <Link to="/arduino-basics" className="text-primary font-semibold">
               Fundamentos
             </Link>
           </li>
           <li>
-            <Link to="/programming-arduino" className="text-blue-500 hover:text-blue-700">
+            <Link to="/programming-arduino" className="text-primary font-semibold">
               Programación
             </Link>
           </li>
           <li>
-            <Link to="/examples" className="text-blue-500 hover:text-blue-700">
+            <Link to="/examples" className="text-primary font-semibold">
               Ejemplos
             </Link>
           </li>
           <li>
-            <Link to="/components" className="text-blue-500 hover:text-blue-700">
+            <Link to="/components" className="text-primary font-semibold">
               Componentes
             </Link>
           </li>
